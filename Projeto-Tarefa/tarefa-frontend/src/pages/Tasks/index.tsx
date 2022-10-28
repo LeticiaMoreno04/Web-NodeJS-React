57d2ff12a1531c091d9b78a07f81b0e1c4e5d69e
LeticiaMoreno04 <lelemoreno24@gmail.com>
2022-10-28 20:47:11 -0300

    Atividade 28/10

--- Projeto-Tarefa/tarefa-frontend/src/pages/Tasks/index.tsx
+++ Projeto-Tarefa/tarefa-frontend/src/pages/Tasks/index.tsx
@@ -2,8 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { Table, Button } from 'react-bootstrap';
 import api from '../../services/api';
 import moment from 'moment';
+import { useNavigate } from 'react-router-dom';
+import './index.css';
 
-interface ITask{
+interface ITask {
     id: number;
     title: string;
     description: string;
@@ -15,6 +17,7 @@ interface ITask{
 const Tasks: React.FC = () => {
 
     const [tasks, setTasks] = useState<ITask[]>([])
+    const history = useNavigate()
 
     useEffect(() => {
         loadTasks()
@@ -26,24 +29,48 @@ const Tasks: React.FC = () => {
         setTasks(response.data)
     }
 
-    function formatDate(date: Date){
+    function formatDate(date: Date) {
         return moment(date).format('DD/MM/YYYY')
     }
 
+    function newTask() {
+        history('/tarefas_cadastro')
+    }
+
+    function editTask(id: number) {
+        history(`/tarefas_cadastro/${id}`)
+    }
+
+    function viewTask(id: number) {
+        history(`/tarefas/${id}`)
+    }
+
+    async function finishedTask(id: number) {
+        await api.patch(`/tasks/${id}`)
+        loadTasks()
+    }
+
+    async function deleteTask(id: number){ 
+        await api.delete(`/tasks/${id}`) 
+        loadTasks() 
+    } 
+       
+
     return (
-        
+
         <div className="container">
             <br />
             <h1>Página de Tarefas</h1>
+            <Button variant="dark" size="sm" onClick={newTask}>Nova Tarefa</Button>
             <br />
             <Table striped bordered hover className="text-center">
                 <thead>
                     <tr>
-                    <th>ID</th>
-                    <th>Título</th>
-                    <th>Data de Atualização</th>
-                    <th>Status</th>
-                    <th>Ações</th>
+                        <th>ID</th>
+                        <th>Título</th>
+                        <th>Data de Atualização</th>
+                        <th>Status</th>
+                        <th>Ações</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -55,10 +82,10 @@ const Tasks: React.FC = () => {
                                 <td>{formatDate(task.updated_at)}</td>
                                 <td>{task.finished ? "Finalizado" : "Pendente"}</td>
                                 <td>
-                                    <Button size="sm" variant="primary">Editar</Button>{' '}
-                                    <Button size="sm" variant="success">Finalizar</Button>{' '}
-                                    <Button size="sm" variant="warning">Visualizar</Button>{' '}
-                                    <Button size="sm" variant="danger">Remover</Button>{' '}
+                                    <Button size="sm" disabled={task.finished} variant="primary" onClick={() => editTask(task.id)}>Editar</Button>{' '}
+                                    <Button size="sm" disabled={task.finished} variant="success" onClick={() => finishedTask(task.id)}>Finalizar</Button>{' '}
+                                    <Button size="sm" variant="warning" onClick={() => viewTask(task.id)}>Visualizar</Button>{' '}
+                                    <Button size="sm" variant="danger" onClick={() => deleteTask(task.id)}>Remover</Button>{' '}
                                 </td>
                             </tr>
                         ))
--- Projeto-Tarefa/tarefa-frontend/src/routes.tsx
+++ Projeto-Tarefa/tarefa-frontend/src/routes.tsx
@@ -3,12 +3,17 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Home from './pages/Home';
 import Tasks from './pages/Tasks';
+import TasksForm from'./pages/Tasks/forms';
+import TasksDetail from'./pages/Tasks/Detail';
 
 const Rotas: React.FC = () => {
     return(
         <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/tarefas" element={<Tasks/>} />
+            <Route path="/tarefas_cadastro" element ={<TasksForm/>} /> 
+            <Route path="/tarefas_cadastro/:id" element ={<TasksForm/>} />
+            <Route path="/tarefas/:id" element={<TasksDetail/>} /> 
         </Routes>
     );
 }
